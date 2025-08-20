15cfad7f4657d27625babcdcc1c9f4b4d3656855
Liu-Kindy <Unofatira@gmail.com>
2025-08-19 22:36:40 -0700

    L

---
+++ U.java
@@ -0,0 +1,26 @@
+public class U {
+    public static void main(String[] args) {
+
+        String nama1="Uno Fathir Ar Royyan Kindy";
+        String kelas="X PPLG";
+        String alamat="Concrong Rogojampi";
+        String hobi="Bermain HP";
+        String jenis="Laki-Laki";
+        String cita="Nganggur";
+        Boolean hubungan=false;
+        int tinggi=155;
+        int berat=40;
+
+        System.out.println("Nama: "+nama1);
+        System.out.println("Kelas: "+kelas);
+        System.out.println("Alamat: "+alamat);
+        System.out.println("Hobi: "+hobi);
+        System.out.println("tinggi: "+tinggi);
+        System.out.println("Berat: "+berat+"kg");
+        System.out.println("Gender: "+jenis);
+        System.out.println("Cita-Cita: "+cita);
+        System.out.println("Status: "+hubungan);
+        
+
+    }
+}
