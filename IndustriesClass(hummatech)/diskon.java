dc2da6f55bcf95b9804279721f4ddc268825fac6
Liu-Kindy <Unofatira@gmail.com>
2025-09-09 12:55:53 +0700

    Add files via upload

---
+++ IndustriesClass(hummatech)/diskon.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class diskon {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        double Belanja;
+        double uang;
+        double diskon = 0;
+
+        System.out.print("Inputkan Total Belanja Anda : ");
+        Belanja = a.nextDouble();
+
+        if (Belanja >= 100000) {
+            System.out.println("selamat anda mendapatkan diskon 20%");
+            diskon = 0.20;
+        } else if (Belanja >= 50000) {
+            System.out.println("Selamat Anda Mendapatkan Diskon 10%");
+            diskon = 0.10;
+        } else if (Belanja >= 20000) {
+            System.out.println("selamat anda mendapatkan diskon 5%");
+            diskon = 0.05;
+        } else {
+            System.out.println("Anda tidak mendapat diskon apapun");
+        }
+
+        System.out.print("Berapa banyak uang yang di berikan: ");
+        uang = a.nextDouble();
+        double total = Belanja * diskon;
+        double totalb = Belanja - total;
+        System.out.println("total yang harus di bayar: " + totalb);
+
+        if (totalb > uang) {
+            System.out.println("uang anda kurang");
+            double tambahan = totalb - uang;
+            System.out.println("Uang anda kurang" + tambahan);
+        } else if (totalb < uang) {
+            System.out.println("uang anda lebih");
+            double kembalian = uang - totalb;
+            System.out.println("kembaliannya " + kembalian);
+            System.out.println("terimaksih telah berbelanja");
+        } else {
+            System.out.println("uang pas");
+            System.out.println("terimaksih telah berbelanja");
+        }
+
+    }
+}
