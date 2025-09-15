8669f46b9b5a273528b8df9a1624ce637cd1428b
Liu-Kindy <Unofatira@gmail.com>
2025-09-15 14:58:19 +0700

    Add files via upload

---
+++ IndustriesClass(hummatech)/variableNumerik.java
@@ -0,0 +1,17 @@
+public class variableNumerik {
+    public static void main(String[] args) {
+    int berat=56;
+    int tinggi=160;
+    int uangDompet=30000;
+    int uangRekening=2100000000;
+    long nilaiInvestasi=30000000000L;
+    double jarak=5.5;
+    System.out.println("Berat Badan: "+berat+"Kg");
+    System.out.println("Tinggi: "+tinggi+"cm");
+    System.out.printf("Uang di Dompet: Rp %,d%n", uangDompet);
+    System.out.printf("Uang di Rekening: Rp %,d%n", uangRekening);
+    System.out.printf("Nilai Investasi: Rp %,d%n", nilaiInvestasi);
+    System.out.println("Jarak Sekolah: "+jarak+"km");
+    
+    }
+}
